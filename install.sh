fee4d16bbe29269def4c7d271037326e32c22a93
GiacomoComitani <giacomo.comitani@studenti.unimi.it>
2025-11-06 22:48:02 +0100

    refactor install.sh to enhance package management and streamline Zsh plugin installation

--- install.sh
+++ install.sh
@@ -5,13 +5,8 @@ DOTFILES_DIR=$HOME/dotfiles
 CONFIG_DIR=$HOME/.config
 
 # --- Elenco Pacchetti Personalizzato (Basato sulla TUA Struttura) ---
-# Pacchetti "Piatti" (gestiti da COPIA)
-PACKAGES_FLAT="rofi gtk-3.0 gtk-4.0 wallust"
-
-# Pacchetti "Annidati" (gestiti da COPIA MANUALE)
+PACKAGES_FLAT="rofi gtk-3.0 gtk-4.0 wallust fontconfig"
 PACKAGES_NESTED="hypr kitty nvim swaync waybar"
-
-# Pacchetti "Home" (gestiti da STOW)
 PACKAGES_HOME="zsh"
 
 # --- 1. Controllo Essenziale ---
@@ -25,27 +20,19 @@ if ! command -v stow &> /dev/null; then
 fi
 
 echo "--- 🧹 Fase 1: Pulizia dei Conflitti (Definitiva) ---"
-# Rimuove le cartelle locali E i file di conflitto
-
 function clean_config {
     echo "Pulizia $1..."
     rm -rf "$CONFIG_DIR/$1"
 }
-
-# Pulizia di tutte le cartelle che verranno gestite
-for pkg in $PACKAGES_FLAT; do
-    clean_config "$pkg"
-done
-for pkg in $PACKAGES_NESTED; do
-    clean_config "$pkg"
-done
-rm -rf "$HOME/Pictures/wallpapers" # Pulizia V21
-
-# Pulizia Zsh (INCLUSO .p10k.zsh e .oh-my-zsh che causavano i conflitti)
+for pkg in $PACKAGES_FLAT; do clean_config "$pkg"; done
+for pkg in $PACKAGES_NESTED; do clean_config "$pkg"; done
+rm -rf "$HOME/Pictures/wallpapers"
 echo "Pulizia file Zsh..."
-rm -f "$HOME/.zshrc"
-rm -f "$HOME/.p10k.zsh"
-rm -rf "$HOME/.oh-my-zsh"
+rm -f "$HOME/.zshrc" "$HOME/.p10k.zsh"; rm -rf "$HOME/.oh-my-zsh"
+
+# --- NUOVA PULIZIA FONT ---
+echo "Pulizia cache font locali..."
+rm -rf "$HOME/.local/share/fonts"
 
 echo "Pulizia completata. Le destinazioni sono pronte."
 
@@ -60,7 +47,7 @@ for pkg in $PACKAGES_NESTED; do
 done
 
 # 2.2. Installazione Pacchetti Piatti (COPIA MANUALE)
-echo "Installazione Pacchetti Piatti (Copia Manuale: Rofi, Wallust, GTK...)"
+echo "Installazione Pacchetti Piatti (Copia Manuale: Rofi, Wallust, GTK, Fontconfig...)"
 for pkg in $PACKAGES_FLAT; do
     echo "-> Copiando (piatto) $pkg"
     mkdir -p "$CONFIG_DIR/$pkg"
@@ -72,44 +59,41 @@ echo "Installazione Sfondi (Copia Manuale in ~/.config/hypr/wallpaper)..."
 mkdir -p "$CONFIG_DIR/hypr/wallpaper"
 cp -r "$DOTFILES_DIR"/wallpapers/* "$CONFIG_DIR/hypr/wallpaper/"
 
-echo -e "\n--- 🚀 Fase 3: Post-Installazione Zsh (Ordine Corretto) ---"
+# 2.4. Installazione Zsh (STOW - L'unica eccezione)
+echo "Installazione Zsh..."
+stow -t "$HOME" $PACKAGES_HOME
 
-# 3.1. Installazione Zsh Plugin (PRIMA di stow)
+# --- NUOVA FASE: INSTALLAZIONE FONT ---
+# 2.5. Installazione Font Locali
+echo "Installazione Font Locali (Nerd Fonts)..."
+mkdir -p "$HOME/.local/share/fonts"
+cp -r "$DOTFILES_DIR"/fonts/* "$HOME/.local/share/fonts/"
+
+echo -e "\n--- 🚀 Fase 3: Post-Installazione e Permessi ---"
+
+# 3.1. Installazione Zsh Plugin
 echo "Installazione Zsh Plugins e Framework..."
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/oh-my-zsh/master/tools/install.sh)" "" --unattended
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
-
-# --- NUOVA CORREZIONE (V28): Installa il plugin Autosuggestions mancante ---
-echo "Installazione Zsh Autosuggestions..."
 git clone https://github.com/zsh-users/zsh-autosuggestions.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
-
-# 3.2. Installazione Zsh (STOW - DOPO i plugin)
-echo "Installazione configurazione Zsh (Stow)..."
-rm -f "$HOME/.zshrc" # Rimuovi il file .zshrc di default creato da Oh My Zsh
-stow -t "$HOME" $PACKAGES_HOME
-if [ $? -ne 0 ]; then
-    echo "ERRORE: Installazione Zsh (Stow) fallita."
-    exit 1
-fi
-
-# 3.3. Correzione Permessi Script
+# 3.2. Correzione Permessi Script
 echo "Impostazione permessi per gli script..."
 chmod +x "$CONFIG_DIR"/hypr/scripts/*
 chmod +x "$CONFIG_DIR"/hypr/UserScripts/*
 chmod +x "$CONFIG_DIR"/hypr/initial-boot.sh
 
+# 3.3. Ricostruisci la cache dei font (FC-CACHE)
+echo "Ricostruzione della cache dei font (fc-cache)..."
+fc-cache -f -v
+
 # 3.4. Imposta Zsh come shell di default
 if [ -f /usr/bin/zsh ]; then
     echo "---"
-    echo "ATTENZIONE: Lo script ora tenterà di impostare Zsh come shell di default."
-    echo "Inserisci la password di '$USER' (la tua password di login) quando richiesta."
+    echo "ATTENZIONE: Inserisci la password di '$USER' (la tua password di login) per impostare Zsh."
     echo "---"
     chsh -s /usr/bin/zsh
-    if [ $? -ne 0 ]; then
-        echo "ERRORE: Cambio shell fallito. Eseguilo manualmente: chsh -s /usr/bin/zsh"
-    fi
 else
     echo "ATTENZIONE: /usr/bin/zsh non trovato."
 fi
